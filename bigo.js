1a904534a8feebfdb01b1fdd22572ee833eeb05d
TausifM <nisargcontact2020@gmail.com>
2022-05-22 11:38:57 +0530

    Other Method of Code

--- bigo.js
+++ bigo.js
@@ -3,11 +3,15 @@
 // Define the Time Complexcity of that algorithm and determine
 // what the lowest possible time complexcity is for this problem.
 
-// function sumNumbers(numbers){
-//     return numbers[0] + numbers[1] + numbers[2] // O(1) == Constant Time Complexcity
-// }
+// ==========================================================================================
 
-// console.log(sumNumbers([1,3,10]))
+function sumNumbers(numbers){
+    return numbers[0] + numbers[1] + numbers[2] // O(1) == Constant Time Complexcity
+}
+
+console.log(sumNumbers([1,3,10]))
+
+// ==========================================================================================
 
 // numbers = [1,3,10]
 function sumNumbers2(numbers) {
@@ -20,3 +24,11 @@ function sumNumbers2(numbers) {
 }
 
 console.log(sumNumbers2([1, 3, 10])); // O(n) Linear Time Complexcity
+
+// ==========================================================================================
+
+function sumNumbers3(numbers) {
+    return numbers.reduce((sum, curNum) => sum + curNum, 0) // Reduce also gives Linear Time Complexcity
+  }
+
+  console.log(sumNumbers3([1, 3, 10]))
